cbf4c77300c0ba73c804671d1b4a04f6c12d4990
Aliaksandr Falei <aliaksandr.fa.mi.re.do@gmail.com>
2023-03-08 11:43:51 +0100

    Added api test

--- tests/Unit/CommissionerTest.php
+++ tests/Unit/CommissionerTest.php
@@ -2,16 +2,22 @@
 
 namespace Commissioner\Tests\Unit;
 
+use Commissioner\BinNumber\ApiBinNumberCountryResolver;
 use Commissioner\BinNumber\BinNumber;
 use Commissioner\BinNumber\BinNumberCountryResolver;
+use Commissioner\BinNumber\BinNumberCountryResolverException;
 use Commissioner\Commission\Commission;
 use Commissioner\Commissioner;
+use Commissioner\Exchange\ApiExchangeRateProvider;
 use Commissioner\Exchange\ExchangeRateProvider;
+use Commissioner\Exchange\ExchangeRateProviderException;
 use Commissioner\Transaction\Currency;
 use Commissioner\Transaction\Repository\TransactionsRepository;
 use Commissioner\Transaction\Transaction;
 use Commissioner\Transaction\TransactionCollection;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpClient\MockHttpClient;
+use Symfony\Component\HttpClient\Response\MockResponse;
 
 
 /**
@@ -147,5 +153,123 @@ class CommissionerTest extends TestCase
         $this->assertCount(1, $commissions);
         $this->assertEquals('EUR', $commissions[0]->currency()->code());
         $this->assertEquals(90.91, $commissions[0]->ceil(2));
+    }
+
+    /**
+     * @test
+     * @return void
+     */
+    public function Should_ThrowException_When_RequestToBinNumberApiFailed(): void
+    {
+        $this->expectException(BinNumberCountryResolverException::class);
+        $this->expectExceptionMessageMatches("/^Failed to get exchange rates.*/");
+
+        $http = new MockHttpClient([
+            new MockResponse(json_encode([]), ['http_code' => 500])
+        ]);
+
+        $this->binNumberCountryResolver = new ApiBinNumberCountryResolver($http, '');
+
+        $commissioner = new Commissioner(
+            $this->transactionsRepository,
+            $this->binNumberCountryResolver,
+            $this->exchangeRateProvider,
+            $this->commissions
+        );
+
+        $commissioner->charge();
+    }
+
+    /**
+     * @test
+     * @return void
+     */
+    public function Should_ThrowException_When_BinNumberApiDidntReturnBinNumber(): void
+    {
+        $this->expectException(BinNumberCountryResolverException::class);
+        $this->expectExceptionMessage("Bin number not found.");
+
+        $http = new MockHttpClient([
+            new MockResponse(json_encode(['test' => true]), ['http_code' => 200])
+        ]);
+
+        $this->binNumberCountryResolver = new ApiBinNumberCountryResolver($http, '');
+
+        $commissioner = new Commissioner(
+            $this->transactionsRepository,
+            $this->binNumberCountryResolver,
+            $this->exchangeRateProvider,
+            $this->commissions
+        );
+
+        $commissioner->charge();
+    }
+
+    /**
+     * @test
+     * @return void
+     */
+    public function Should_ThrowException_When_RequestToExchangeApiFailed(): void
+    {
+        $this->expectException(ExchangeRateProviderException::class);
+        $this->expectExceptionMessageMatches("/^Failed to get exchange rates: .*/");
+
+        $http = new MockHttpClient([
+            new MockResponse(json_encode([]), ['http_code' => 500])
+        ]);
+
+        $this->transactionsRepository = $this->createMock(TransactionsRepository::class);
+        $this->transactionsRepository
+            ->method('getTransactionsToProcess')
+            ->willReturn(
+                new TransactionCollection(
+                    new Transaction(new BinNumber('1111'), 100, new Currency('USD'))
+                )
+            )
+        ;
+        $this->exchangeRateProvider = new ApiExchangeRateProvider($http, '', '');
+
+        $commissioner = new Commissioner(
+            $this->transactionsRepository,
+            $this->binNumberCountryResolver,
+            $this->exchangeRateProvider,
+            $this->commissions
+        );
+
+        $commissioner->charge();
+    }
+
+    /**
+     * @test
+     * @return void
+     */
+    public function Should_ThrowException_When_ExchangeRateApiDidntReturnRate(): void
+    {
+        $this->expectException(ExchangeRateProviderException::class);
+        $this->expectExceptionMessage("No exchange rates for this currency pair.");
+
+        $http = new MockHttpClient([
+            new MockResponse(json_encode(['test' => true]), ['http_code' => 200])
+        ]);
+
+        $this->transactionsRepository = $this->createMock(TransactionsRepository::class);
+        $this->transactionsRepository
+            ->method('getTransactionsToProcess')
+            ->willReturn(
+                new TransactionCollection(
+                    new Transaction(new BinNumber('1111'), 100, new Currency('USD'))
+                )
+            )
+        ;
+        $this->exchangeRateProvider = new ApiExchangeRateProvider($http, '', '');
+
+        $commissioner = new Commissioner(
+            $this->transactionsRepository,
+            $this->binNumberCountryResolver,
+            $this->exchangeRateProvider,
+            $this->commissions
+        );
+
+        $commissioner->charge();
     }
 }
